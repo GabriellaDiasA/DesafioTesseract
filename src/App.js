ead71c3a84c395e7e145202b4457f869fef70ba5
Gabriella Dias <75454876+GabriellaDiasA@users.noreply.github.com>
2021-03-29 16:48:19 -0300

    Restante das operações Fetch realizadas, cada uma com seu state respectivo, mantendo os IDs relacionados

--- src/App.js
+++ src/App.js
@@ -8,18 +8,59 @@ import Footer from './components/Footer.js';
 function App() {
 
 	const [members, setMembers] = useState([]);
+	const [repos, setRepos] = useState([]);
+	const [followers, setFollowers] = useState([]);
+	const [createDate, setCreated] = useState([]);
 
 	useEffect(() => {
 		async function fetchData(){
+			//Fetch inicial
 			const response = await fetch(`https://api.github.com/orgs/grupotesseract/public_members`)
 			.catch(error => {console.log(error)});
-			const data = await response.json();
+			let data = await response.json();
 			setMembers(data);
+
+			//Fetch repositórios
+			let reposTemp = [];
+			for(let member in data){
+				const response = await fetch(data[member].repos_url)
+				.catch(error => {console.log(error)});
+				let dataRepos = await response.json();
+				let reposNum = dataRepos.length;
+				let newRepos = { id: data[member].id, reposNum: reposNum };
+				reposTemp = [...reposTemp, newRepos];
+			}
+			setRepos(reposTemp);
+
+			//Fetch followers
+			let followersTemp = [];
+			for(let member in data){
+				const response = await fetch(data[member].followers_url)
+				.catch(error => {console.log(error)});
+				let dataFollowers = await response.json();
+				let followersNum = dataFollowers.length;
+				let newFollowers = { id: data[member].id, followersNum: followersNum };
+				followersTemp = [...followersTemp, newFollowers];
+			}
+			setFollowers(followersTemp);
+
+			//Fetch data de criação de conta
+			let createTemp = [];
+			for(let member in data){
+				const response = await fetch(data[member].url)
+				.catch(error => {console.log(error)});
+				let dataCreate = await response.json();
+				let created_at = dataCreate.created_at.slice(0, 10);
+				let newCreate = { id: data[member].id, created_at: created_at };
+				createTemp = [...createTemp, newCreate];
+			}
+			setCreated(createTemp)
 		}
 		fetchData();
 	}, []);
 
 	const toggleSelection = (id) => {
+		console.log(members[0]);
 		setMembers(members.map((member) => member.id === id ? { ...member, selected: !member.selected } : member));
 	};
 
@@ -29,7 +70,10 @@ function App() {
 			<Header title="Desafio Tesseract"/>
 			<Tesseract
 				onToggle={toggleSelection}
-				members={members}/>
+				members={members}
+				repos={repos}
+				followers={followers}
+				date={createDate}/>
 			<Footer />
 		</div>
   	);
